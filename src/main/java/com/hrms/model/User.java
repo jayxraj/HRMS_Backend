1d8be70a771b02b850c2bbe3a5b143dc2f92ca79
Jayaraj T <jayarajt447@gmail.com>
2025-03-06 11:49:39 +0530

    first commit

---
+++ src/main/java/com/hrms/model/User.java
@@ -0,0 +1,15 @@
+package com.hrms.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role = "USER";
+}
